4fa656328386afe3668cd419fe5eb513fca37896
NachoToast <32235595+NachoToast@users.noreply.github.com>
2026-03-29 19:15:25 +1300

    update direct steam lookup endpoint

--- src/routes/lookup/getSteamDirect.ts
+++ src/routes/lookup/getSteamDirect.ts
@@ -1,13 +1,18 @@
-import { steamUsersDb } from "@/databases/steamUsers";
-import type { SteamId64, SteamUserWithTimes } from "@/shared/types/SteamUser";
+import { joinUsersDiscord } from "@/databases/joins/joinUsersDiscords";
+import type { SteamId64, SteamUserWithTimesAndDiscord } from "@/shared/types/SteamUser";
 import { AuthScope } from "@/types/Express/AuthScope";
 import type { Endpoint } from "@/types/Express/Endpoint";
 
-export const getSteamDirect: Endpoint<void, SteamUserWithTimes[], void, { ids: SteamId64[] }> = {
+export const getSteamDirect: Endpoint<
+    void,
+    SteamUserWithTimesAndDiscord[],
+    void,
+    { ids: SteamId64[] }
+> = {
     method: "get",
     path: "/lookup/steam-direct",
     auth: AuthScope.None,
     async handleRequest({ req, timer }) {
-        return await steamUsersDb.getSteamUsersDirect(req.query.ids, timer);
+        return await joinUsersDiscord(req.query.ids, timer);
     },
 };
