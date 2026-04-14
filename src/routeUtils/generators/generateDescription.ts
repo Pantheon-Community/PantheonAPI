4728dc29823cfed72861dfad94810ea9193081f7
NachoToast <32235595+NachoToast@users.noreply.github.com>
2026-04-14 12:19:38 +1200

    more auth info in docs

--- src/routeUtils/generators/generateDescription.ts
+++ src/routeUtils/generators/generateDescription.ts
@@ -1,4 +1,8 @@
+import { GeneralPermissionInfo } from "@/shared/constants/permissions/GeneralPermissionsInfo";
+import { UserPermissionsInfo } from "@/shared/constants/permissions/UserPermissionsInfo";
 import type { OAS } from "@/shared/global/OAS";
+import { split } from "@/shared/utils/bitfieldHelpers";
+import { AuthScope } from "@/types/Express/AuthScope";
 import type { AnyEndpoint } from "@/types/Express/Endpoint";
 import { readFileSync } from "node:fs";
 import { basename, join, relative, sep } from "node:path";
@@ -19,7 +23,13 @@ export function generateEndpointDescription(
 ): Pick<OAS.Operation, "description" | "tags"> {
     const { description: initialDescription, source, tag: overrideTag } = endpoint;
 
-    const description = `${initialDescription}\n\n[${basename(source)}](https://github.com/Pantheon-Community/PantheonAPI/blob/main/src/${relative(rootDir, source).replaceAll(sep, "/")})`;
+    const descriptionParts: string[] = [
+        initialDescription,
+        getAuthDescription(endpoint),
+        `[${basename(source)}](https://github.com/Pantheon-Community/PantheonAPI/blob/main/src/${relative(rootDir, source).replaceAll(sep, "/")})`,
+    ];
+
+    const description = descriptionParts.join("\n\n");
 
     const tag = overrideTag ?? relative(routesDir, source).split(sep).at(0);
 
@@ -29,3 +39,47 @@ export function generateEndpointDescription(
 
     return { description, tags: [tag] };
 }
+
+function getAuthDescription(endpoint: AnyEndpoint): string {
+    const { auth } = endpoint;
+
+    const title = `Authorization Level: **${AuthScope[auth]}**`;
+
+    const description = AUTH_SCOPE_DESCRIPTIONS[auth];
+
+    if (auth !== AuthScope.Permission) {
+        return `${title} - ${description}`;
+    }
+
+    const subDescriptions: string[] = [];
+
+    const { permissions } = endpoint;
+
+    const { generalPermissions, userPermissions } = permissions;
+
+    if (generalPermissions) {
+        const perms = split(generalPermissions);
+
+        subDescriptions.push(
+            `- General Permissions (${perms.length}): ${perms.map((x) => `\`${GeneralPermissionInfo[x].name}\``).join(", ")}`,
+        );
+    }
+
+    if (userPermissions) {
+        const perms = split(userPermissions);
+
+        subDescriptions.push(
+            `- User Permissions (${perms.length}): ${perms.map((x) => `\`${UserPermissionsInfo[x].name}\``).join(", ")}`,
+        );
+    }
+
+    return `${title} - ${description}\n${subDescriptions.join("\n")}`;
+}
+
+const AUTH_SCOPE_DESCRIPTIONS: Record<AuthScope, string> = {
+    [AuthScope.None]: "Anyone can call this endpoint.",
+    [AuthScope.Session]: "Only logged-in users can call this endpoint.",
+    [AuthScope.Permission]: "Only logged-in users with certain permissions can call this endpoint.",
+    [AuthScope.Plugin]:
+        "Only our SCP:SL plugins and other first-party services can call this endpoint.",
+};
