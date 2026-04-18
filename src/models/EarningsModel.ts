5adfbdeb2a98c7e35eb73afe6a39eaf726789adb
NachoToast <32235595+NachoToast@users.noreply.github.com>
2026-04-18 16:54:30 +1200

    login streaks

--- src/models/EarningsModel.ts
+++ src/models/EarningsModel.ts
@@ -1,12 +1,9 @@
 import { pgUnsafe } from "@/global/pg";
 import type { PluginTokenId } from "@/shared/types/PluginToken";
 import type { SteamUserModel } from "./SteamUserModel";
-import type { UserModel } from "./UserModel";
 
 export interface EarningsModel {
     id: string;
-
-    user_id: UserModel["id"];
 
     steam_id: SteamUserModel["id"];
 
@@ -21,11 +18,12 @@ export async function createEarningsTable(): Promise<void> {
     await pgUnsafe(`
         CREATE TABLE IF NOT EXISTS earnings (
             id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-            user_id TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
             steam_id TEXT NOT NULL REFERENCES steam_users(id) ON DELETE CASCADE,
             amount INTEGER NOT NULL,
             made_by INTEGER REFERENCES plugin_tokens(id) ON DELETE SET NULL,
             made_at TIMESTAMP NOT NULL DEFAULT NOW()
         );
+
+        ALTER TABLE earnings DROP COLUMN IF EXISTS user_id;
     `);
 }
--- src/routes/economy/earnings/postEarnings.ts
+++ src/routes/economy/earnings/postEarnings.ts
@@ -1,6 +1,6 @@
 import { pg } from "@/global/pg";
 import type { EarningsModel } from "@/models/EarningsModel";
-import type { UserModel } from "@/models/UserModel";
+import type { SteamUserModel } from "@/models/SteamUserModel";
 import type { PluginTokenId } from "@/shared/types/PluginToken";
 import { EARNINGS_REQUEST, type EarningsRequest } from "@/shared/types/Requests/EarningsRequest";
 import { AuthScope } from "@/types/Express/AuthScope";
@@ -23,80 +23,159 @@ export const postEarnings: Endpoint<EarningsRequest[]> = {
     pathParams: null,
     queryParams: null,
     async handleRequest({ req, timer, plugin }) {
+        using _ = timer.create("updateSteamUsers");
+
         const earnings = req.body;
 
         await Promise.all([
-            increaseBalances(earnings, timer).then((users) =>
-                addEarningRecords(users, plugin, timer),
-            ),
-            upsertAnalytics(earnings, timer),
+            addEarningRecords(earnings, plugin, timer),
+            ...earnings.map(updateSteamUser),
         ]);
     },
 };
 
-interface EarningFull extends EarningsRequest {
-    id: UserModel["id"];
-}
-
-async function increaseBalances(
-    earnings: EarningsRequest[],
-    timer: ServerTimer,
-): Promise<EarningFull[]> {
-    using _ = timer.create("increaseBalances");
-
-    const users = await Promise.all(earnings.map(increaseBalanceFor));
-
-    return users.flatMap((x) => x);
-}
-
-async function increaseBalanceFor(earning: EarningsRequest): Promise<EarningFull[]> {
-    const { steamId, balanceInc } = earning;
-
-    const users = await pg<Pick<UserModel, "id">[]>`
-        UPDATE steam_users
-        SET
-            balance = balance + ${balanceInc},
-            lifetime_balance = lifetime_balance + ${balanceInc}
-        WHERE id = ${steamId}
-        RETURNING id
-    `;
-
-    return users.map((x) => Object.assign(x, earning));
-}
-
-async function upsertAnalytics(earnings: EarningsRequest[], timer: ServerTimer): Promise<void> {
-    using _ = timer.create("upsertAnalytics");
-
-    await Promise.all(earnings.map(upsertAnalyticsFor));
-}
-
-async function upsertAnalyticsFor(earning: EarningsRequest): Promise<void> {
-    const { steamId, username } = earning;
-
-    await pg`
-        INSERT INTO steam_users (id, username, first_seen_at, last_seen_at, times_seen)
-        VALUES (${steamId}, ${username}, NOW(), NOW(), 1)
-        ON CONFLICT (id) DO UPDATE SET
-            username = ${username},
-            first_seen_at = COALESCE(steam_users.first_seen_at, NOW()),
-            last_seen_at = NOW(),
-            times_seen = steam_users.times_seen + 1
-    `;
-}
-
 async function addEarningRecords(
-    users: EarningFull[],
+    earnings: EarningsRequest[],
     plugin: PluginTokenId,
     timer: ServerTimer,
 ): Promise<void> {
     using _ = timer.create("addEarningRecords");
 
-    const values = users.map<Partial<EarningsModel>>((x) => ({
-        user_id: x.id,
+    const values = earnings.map<Partial<EarningsModel>>((x) => ({
         steam_id: x.steamId,
         amount: x.balanceInc,
         made_by: plugin,
     }));
 
     await pg`INSERT INTO earnings ${sql(values)}`;
+}
+
+async function updateSteamUser(earning: EarningsRequest): Promise<void> {
+    const { steamId, username, balanceInc, groupName } = earning;
+
+    let signinBonus = getDailySignInBonus(groupName);
+
+    const [steamUser] = await pg<
+        [Pick<SteamUserModel, "last_login_bonus_given_at" | "login_streak">]
+    >`
+        INSERT INTO steam_users (
+            id,
+            username,
+            group_name,
+            first_seen_at,
+            last_seen_at,
+            times_seen,
+            balance,
+            lifetime_balance
+        ) VALUES (
+            ${steamId},
+            ${username},
+            ${groupName},
+            NOW(),
+            NOW(),
+            1,
+            ${balanceInc},
+            ${balanceInc}
+        ) ON CONFLICT (id) DO UPDATE SET
+            username = ${username},
+            group_name = ${groupName},
+            first_seen_at = COALESCE(steam_users.first_seen_at, NOW()),
+            last_seen_at = NOW(),
+            times_seen = steam_users.times_seen + 1,
+            balance = steam_users.balance + ${balanceInc},
+            lifetime_balance = steam_users.lifetime_balance + ${balanceInc}
+        RETURNING last_login_bonus_given_at, login_streak
+    `;
+
+    const { last_login_bonus_given_at, login_streak } = steamUser;
+
+    if (last_login_bonus_given_at === null) {
+        console.log("new user");
+
+        await pg`
+            UPDATE steam_users
+            SET
+                balance = balance + ${signinBonus},
+                lifetime_balance = lifetime_balance + ${signinBonus},
+                last_login_bonus_given_at = NOW(),
+                login_streak = 1
+            WHERE id = ${steamId}
+        `;
+    } else if (last_login_bonus_given_at < startOfYesterday()) {
+        // streak broken
+        console.log("streak broken!");
+
+        await pg`
+            UPDATE steam_users
+            SET
+                balance = balance + ${signinBonus},
+                lifetime_balance = lifetime_balance + ${signinBonus},
+                last_login_bonus_given_at = NOW(),
+                login_streak = 1
+            WHERE id = ${steamId}
+        `;
+    } else if (last_login_bonus_given_at < startOfToday()) {
+        // streak increased
+        console.log("streak increased!");
+
+        // +10% earnings per consecutive day, max of +50% (5 days)
+        signinBonus += Math.floor(signinBonus * Math.min(login_streak / 10, 0.5));
+
+        await pg`
+            UPDATE steam_users
+            SET
+                balance = balance + ${signinBonus},
+                lifetime_balance = lifetime_balance + ${signinBonus},
+                last_login_bonus_given_at = NOW(),
+                login_streak = login_streak + 1
+        `;
+    } else {
+        // logged in recently, no streak actions
+        console.log("streak unchanged!");
+    }
+}
+
+function startOfToday(): Date {
+    const today = new Date();
+
+    today.setHours(0, 0, 0);
+
+    return today;
+}
+
+function startOfYesterday(): Date {
+    const d = new Date();
+
+    d.setHours(-24, 0, 0);
+
+    return d;
+}
+
+console.log(
+    `Today is ${startOfToday().toLocaleString("en-NZ")}, Yesterday was ${startOfYesterday().toLocaleString("en-NZ")}`,
+);
+
+function getDailySignInBonus(groupName: string): number {
+    // we will put these in a DB eventually
+    switch (groupName) {
+        case "d5mega":
+        case "d4mega":
+            return 350;
+        case "d3gold":
+            return 275;
+        case "d2silver":
+            return 200;
+        case "d1bronze":
+            return 150;
+        // case "director":
+        // case "manager":
+        // case "supervisor":
+        // case "developer":
+        // case "admin":
+        // case "smoderator":
+        // case "moderator":
+        // case "tmoderator":
+        default:
+            return 50;
+    }
 }
